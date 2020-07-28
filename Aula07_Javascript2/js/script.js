87f79c268e279838383cd0bd8bab993886273e8f
vividerocha <vividerocha@gmail.com>
2020-07-28 10:43:44 -0300

    Nova pasta aula javascript

---
+++ Aula07_Javascript2/js/script.js
@@ -0,0 +1,38 @@
+var positiva = 3;
+var negativa = -5;
+var decimal = 2.5;
+var nome = "Viviane";
+var verdadeiro = true;
+
+console.log(positiva);
+console.log(negativa);
+console.log(decimal);
+console.log(nome);
+console.log(verdadeiro);
+
+var nome = "Viviane";
+var sobrenome = "Rocha";
+
+console.log(nome + " " + sobrenome);
+
+var nome = "Clara";
+console.log(nome);
+
+var nome = "Clara";
+console.log(nome, sobrenome);
+
+var texto1 = "Primeira linha do texto \nSegunda linha do texto \nTerceira Linha do Texto";
+console.log(texto1);
+
+var nulo = null;
+var indefinido;
+
+console.log(nulo);
+console.log(indefinido);
+
+var frutas = ["Pera", "Uva", "Maçã", "Goiaba", "Banana"];
+console.log(frutas);
+
+var text = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam.";
+console.log(text);
+
